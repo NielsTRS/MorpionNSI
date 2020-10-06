7605e65495a83a471169f3f3f677ac7c7fb8502c
NielsTRS <trsniels@gmail.com>
2020-10-06 14:28:03 +0200

    Fix bug

--- libs/core.py
+++ libs/core.py
@@ -14,11 +14,11 @@ class Core:
         if isinstance(x, int) and isinstance(y, int):
             self.x = x
             self.y = y
+            self.surf = pygame.display.set_mode((self.x, self.y))
         else:
-            raise TypeError("La taille de la fenêtre doivent être défini par des nombres")
+            raise TypeError("Les paramètres doivent être définis par des nombres")
 
     def start(self):
-        surf = pygame.display.set_mode((self.x, self.y))
         image = pygame.image.load(r'./assets/images/main_menu.png').convert()
 
         bot_area = pygame.Rect((117, 340), (261, 261))  # left, top, width, height
@@ -27,21 +27,27 @@ class Core:
 
         run = True
         while run:
-            surf.blit(image, (0, 0))
+            self.surf.blit(image, (0, 0))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:  # clic gauche
                         if bot_area.collidepoint(event.pos):
+                            image = self.__setImage('./assets/images/game_screen.png')
                             self.bot()
                         if friend_area.collidepoint(event.pos):
+                            image = self.__setImage('./assets/images/game_screen.png')
                             self.friend()
                         if stranger_area.collidepoint(event.pos):
+                            image = self.__setImage('./assets/images/game_screen.png')
                             self.stranger()
-
+            self.surf.blit(image, (0, 0))
             pygame.display.update()
         pygame.quit()
+
+    def __setImage(self, image: str):
+        return pygame.image.load(fr'{image}').convert()
 
     def bot(self):
         return True
