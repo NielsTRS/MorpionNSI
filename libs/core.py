71b6638cbefafacfb09e865ceba76c5c713298d7
NielsTRS <trsniels@gmail.com>
2020-10-13 11:31:00 +0200

    Add comment

--- libs/core.py
+++ libs/core.py
@@ -6,6 +6,7 @@ import pygame
 class Core:
     def __init__(self, x: int, y: int):
         """
+        Init function to setup the game
         :param x: Taille de la fenêtre en longeur
         :param y: Taille de la fenêtre en largeur
         :type x: int
@@ -83,45 +84,64 @@ class Core:
             raise TypeError("Les paramètres doivent être définis par des nombres")
 
     def start(self):
-
-        self.showIndex()
+        """
+        Main function to start the game
+        :return:
+        """
+        self.__showIndex()
         while self.status:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.status = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:  # clic gauche
-                        if self.bot_image_rect.collidepoint(event.pos):
+                    if event.button == 1:  # left clic
+                        if self.bot_image_rect.collidepoint(event.pos): #bot mode
                             self.__setBackgroundImage('./assets/images/game_screen.png')
                             self.surf.blit(self.bot_image_icn,
                                            (self.bot_sizes['pos_x_icn'], self.bot_sizes['pos_y_icn']))
                             self.surf.blit(self.close_image, (self.close_sizes['pos_x'], self.close_sizes['pos_y']))
 
-                        if self.local_image_rect.collidepoint(event.pos):
+                        if self.local_image_rect.collidepoint(event.pos): #local mode
                             self.__setBackgroundImage('./assets/images/game_screen.png')
                             self.surf.blit(self.local_image_icn,
                                            (self.local_sizes['pos_x_icn'], self.local_sizes['pos_y_icn']))
                             self.surf.blit(self.close_image, (self.close_sizes['pos_x'], self.close_sizes['pos_y']))
 
-                        if self.online_image_rect.collidepoint(event.pos):
+                        if self.online_image_rect.collidepoint(event.pos): #online mode
                             self.__setBackgroundImage('./assets/images/game_screen.png')
                             self.surf.blit(self.online_image_icn,
                                            (self.online_sizes['pos_x_icn'], self.online_sizes['pos_y_icn']))
                             self.surf.blit(self.close_image, (self.close_sizes['pos_x'], self.close_sizes['pos_y']))
 
-                        if self.close_image_rect.collidepoint(event.pos):
-                            self.showIndex()
+                        if self.close_image_rect.collidepoint(event.pos): #return to index
+                            self.__showIndex()
 
             pygame.display.flip()
 
-    def showIndex(self):
+    def __showIndex(self):
+        """
+        Private function to show the index window
+        :return:
+        """
         self.__setBackgroundImage('./assets/images/main_menu.png')
         self.surf.blit(self.bot_image, (self.bot_sizes['pos_x'], self.bot_sizes['pos_y']))
         self.surf.blit(self.local_image, (self.local_sizes['pos_x'], self.local_sizes['pos_y']))
         self.surf.blit(self.online_image, (self.online_sizes['pos_x'], self.online_sizes['pos_y']))
 
     def __loadImage(self, image: str):
+        """
+        Private function to load an image
+        :param image: link of the image
+        :type image: str
+        :return:
+        """
         return pygame.image.load(fr'{image}').convert_alpha()
 
     def __setBackgroundImage(self, image: str):
+        """
+        Show the image in background
+        :param image: link of the image
+        :type image: str
+        :return:
+        """
         self.surf.blit(self.__loadImage(image), (0, 0))
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
-
 import libs.core as core
 
-game = core.Core(1280, 720)
-game.start()
+game = core.Core(1280, 720) #load the game
+game.start() #start the game
